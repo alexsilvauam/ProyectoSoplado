95b58ed29b3accdac7d57cb5fdce414ee8ca658f
Jeff Walters <165819919+jwalters63@users.noreply.github.com>
2024-11-26 19:02:05 -0600

    subir resolución
    subir resolución de una foto

--- Formularios/FormPrincipal.Designer.cs
+++ Formularios/FormPrincipal.Designer.cs
@@ -240,7 +240,7 @@
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(160, 328);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
